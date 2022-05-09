f4ba71ba82c5f40df297f385bc759fb094d21b0f
mmullen89 <matthewmullen89@gmail.com>
2022-05-09 00:50:26 -0400

    Style

--- js/application.js
+++ js/application.js
@@ -2,10 +2,10 @@ var sum = function (acc, x) { return acc + x; };
 
 var updateSubtotal = function(element) {
   var quantity = parseInt($(element).find('.quantity input').val());
-  var price = parseFloat($(element).find('.price').text());
+  var price = parseFloat($(element).find('.price').text().replace(/\$/,""));
 
-  var subtotal = quantity * price;
-  $(element).children('.subtotal').html(subtotal);
+  var subtotal = Math.round((quantity * price)*100)/100;
+  $(element).children('.subtotal').html("$"+subtotal);
   return subtotal;
 }
 
@@ -17,6 +17,7 @@ var updatePrices = function() {
   });
 
   var total = totalPrice.reduce(sum);
+  total = Math.round(total*100)/100;
   $('#totalValue').html(total);
 }
 
@@ -43,7 +44,7 @@ $(document).ready(function () {
 
     $('tbody').append('<tr>'+
     '<td class="name">' + itemName + '</td>' +
-    '<td class = "price">' + itemPrice + '</td>' + 
+    '<td class = "price"> $' + itemPrice + '</td>' + 
     '<td class = "quantity"><input type="number" value="1" /></td>' + 
     '<td class = "subtotal"></td>' + 
     '<td><button class="btn btn-light btn-sm remove">remove</button></td>')
